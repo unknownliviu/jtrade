662b88f0fe47805b6ad6c3c139d4db3b620bd3d0
liviu <unknownliviu@gmail.com>
2013-04-23 11:16:18 +0300

    initial comit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jtrade::Application.config.secret_token = '8bd33e8db4986098e4109943a0b065a21148fe95e6f7f9ea7f1e86e2cf2d8aaf9ac72960d867ed7e8bf8e5e2f52dfdfa8ff5ba223a7c5fce2a20bbba3248c49b'
