8e1498debd404dd493ed83c161e630a85eef197d
Unknownliviu <unknownliviu@gmail.com>
2013-04-26 00:36:56 +0300

    basic photo upload

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,14 @@
 class Item < ActiveRecord::Base
-  attr_accessible :description, :name, :price
+  attr_accessible :description, :name, :price,:photo
   belongs_to :user
 
   validates :name , presence: true, length:{minimum: 3}
   validates :description, presence: true, length:{minimum: 5}
   validates :price, numericality: {greater_than_or_equal_to: 0, less_than: 2000000}
+
+    has_attached_file :photo,                                                  
+      :path => ":rails_root/public/system/:attachment/:id/:style/:filename",    
+      :url => "/system/:attachment/:id/:style/:filename"  
 
   default_scope order: 'items.created_at DESC'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130425182854) do
+ActiveRecord::Schema.define(:version => 20130425212808) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -35,9 +35,13 @@ ActiveRecord::Schema.define(:version => 20130425182854) do
     t.string   "name"
     t.text     "description"
     t.float    "price"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
     t.integer  "user_id"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
   create_table "users", :force => true do |t|
