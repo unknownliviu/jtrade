6ef479b0c721d0900b724e3aed939e51f957b522
Unknownliviu <unknownliviu@gmail.com>
2013-04-25 12:24:44 +0300

    completed user model and crud

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,12 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :name
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :phone, :location
   # attr_accessible :title, :body
 
+  VALID_PHONE_REGEX = /\d{7,15}/
   validates :name , presence: true, length: {maximum: 20, minimum: 3}
+  validates :phone, length: { maximum: 15 }, format: { with: VALID_PHONE_REGEX }
+  validates :location, length: { maximum: 100 }
+
 end
