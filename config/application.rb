4e1c7f6b5b791421018a64525f65150794c66c8c
Unknownliviu <unknownliviu@gmail.com>
2013-04-25 12:40:04 +0300

    heroku fix

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Jtrade
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Added fix by moi
+    config.assets.initialize_on_precompile = false
   end
 end
