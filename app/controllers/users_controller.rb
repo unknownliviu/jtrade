053e3691f629f25c7d649e21940d472ae80c9c1e
Unknownliviu <unknownliviu@gmail.com>
2013-04-25 11:29:05 +0300

    i was an absolute moron=>edit fixed ,signin pretty

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,19 +4,4 @@ class UsersController < ApplicationController
   def show
   end
 
-  def edit
-  	@user = current_user
-  end
-
-  def update
-  	@user = current_user
-    if @user.update_attributes(params[:user])
-      flash[:success] = "Profile updated!"
-      sign_in @user
-      redirect_to @user
-    else
-      render 'edit'
-    end
-  end
-
 end
